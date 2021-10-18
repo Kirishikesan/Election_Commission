f8d79c38934ee589b1f2f6d395e8a11f024e896e
Kirishikesan Kannan <kirishikesan.17@cse.mrt.ac.lk>
2021-10-18 06:43:02 +0530

    CSPRNG implementation

--- electionCommission/src/main/java/com/crypto/electionCommission/mail/MailController.java
+++ electionCommission/src/main/java/com/crypto/electionCommission/mail/MailController.java
@@ -1,12 +1,13 @@
 package com.crypto.electionCommission.mail;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -14,7 +15,6 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +26,7 @@ public class MailController {
    @RequestMapping(value = "/sendemail")
    public String sendEmail() {
 	  try {
-		  sendmail();
+		  sendRandomID();
 	      return "Email sent successfully";
 	  }catch(Exception e) {
 		  System.out.println(e);
@@ -34,12 +34,19 @@ public class MailController {
 	  }
    }   
 
-   private void sendmail() throws AddressException, MessagingException, IOException {
+   private void sendRandomID() throws AddressException, MessagingException, IOException, NoSuchAlgorithmException {
 	   Properties props = new Properties();
 	   props.put("mail.smtp.auth", "true");
 	   props.put("mail.smtp.starttls.enable", "true");
 	   props.put("mail.smtp.host", "smtp.gmail.com");
 	   props.put("mail.smtp.port", "587");
+	   
+	   byte[] seed = SecureRandom.getSeed(32);
+	   SecureRandom CSPRNG = new SecureRandom(seed);
+//	   IntStream intStream = CSPRNG.ints(10);
+	   String randomID=Integer.toString(CSPRNG.nextInt(99999999));
+	   
+
 	   
 	   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 	      protected PasswordAuthentication getPasswordAuthentication() {
@@ -51,7 +58,7 @@ public class MailController {
 
 	   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("krishi.rc97@gmail.com"));
 	   msg.setSubject("RandomID");
-	   msg.setContent("RandomID", "text/html");
+	   msg.setContent(randomID, "text/html");
 	   msg.setSentDate(new Date());
 
 	   MimeBodyPart messageBodyPart = new MimeBodyPart();
